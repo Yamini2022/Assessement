6d8a8019d00aac42e126299cd43da063bce54d61
Yamini2022 <105438142+Yamini2022@users.noreply.github.com>
2022-05-12 19:24:32 +0530

    Assessement

---
+++ example.tf
@@ -0,0 +1,55 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_route53_zone" "example" {
+  name = "google.com"
+}
+
+module "dns_records" {
+  source = "terraformdns/route53-recordsets/aws"
+  # TODO: Constrain to a specific minor version before use in production
+  # version = "~> N.N"
+
+  route53_zone = aws_route53_zone.example
+  recordsets = [
+    {
+      name = "www"
+      type = "A"
+      ttl  = 3600
+      records = [
+        "192.0.2.56",
+      ]
+    },
+    {
+      name = ""
+      type = "MX"
+      ttl  = 3600
+      records = [
+        "1 mail1",
+        "5 mail2",
+        "5 mail3",
+      ]
+    },
+    {
+      name = ""
+      type = "TXT"
+      ttl  = 3600
+      records = [
+        "\"v=spf1 ip4:192.0.2.3 include:backoff.${aws_route53_zone.example.name} -all\"",
+      ]
+    },
+    {
+      name = "_sip._tcp"
+      type = "SRV"
+      ttl  = 3600
+      records = [
+        "10 60 5060 sip1",
+        "10 20 5060 sip2",
+        "10 20 5060 sip3",
+        "20  0 5060 sip4",
+      ]
+    },
+  ]
+}
